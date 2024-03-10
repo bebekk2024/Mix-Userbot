6e8bb51a4d54d8f6e6207c1fdb43ae3f7ac542b2
bebekk2024 <161502478+bebekk2024@users.noreply.github.com>
2024-03-10 21:08:57 +0700

    sangmata.py

--- modular/sangmata.py
+++ modular/sangmata.py
@@ -2,7 +2,7 @@
 """
  Mix-Userbot Open Source . Maintained ? Yes Oh No Oh Yes Ngentot
  
- @ CREDIT : NAN-DEV || Gojo_Satoru
+ @ CREDIT : NAN-DEV
 """
 ################################################################
 
