1ce1e4a77dced1b677240a606f73d63b2f8d7639
bebekk2024 <161502478+bebekk2024@users.noreply.github.com>
2024-03-13 22:45:04 +0700

    __init__.py

--- modular/__init__.py
+++ modular/__init__.py
@@ -1,13 +1,20 @@
-from glob import glob
-from os.path import basename, dirname, isfile
+from team.nandev.class_log import LOGGER
 
 
 def import_modular():
-    mod_paths = glob(f"{dirname(__file__)}/*.py")
-    return sorted(
-        [
-            basename(f)[:-3]
-            for f in mod_paths
-            if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
-        ]
-    )
+    import glob
+    from os.path import basename, dirname, isfile
+
+    mod_paths = glob.glob(dirname(file) + "/*.py")
+    all_modules = [
+        basename(f)[:-3]
+        for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("init.py")
+    ]
+
+    return all_modules
+
+
+USER_MOD = sorted(import_modular())
+# LOGGER.info("Userbot module loaded: %s", str(USER_MOD))
+all = USER_MOD + ["USER_MOD"]
